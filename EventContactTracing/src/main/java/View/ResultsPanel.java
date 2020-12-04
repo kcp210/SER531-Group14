59f954d8d5f7952c159ae6843a446be23a351f6a
Kaysi Pilcher <kaysi.pilcher@gmail.com>
2020-12-03 19:55:01 -0700

    Removed secondary contact table

--- EventContactTracing/src/main/java/View/ResultsPanel.java
+++ EventContactTracing/src/main/java/View/ResultsPanel.java
@@ -11,35 +11,36 @@ public class ResultsPanel extends JPanel {
     private JLabel immediateContactHighRiskLabel;
     private JLabel immediateContactMediumRiskLabel;
     private JLabel immediateContactLowRiskLabel;
-    private JLabel secondaryContactHighRiskLabel;
-    private JLabel secondaryContactMediumRiskLabel;
-    private JLabel secondaryContactLowRiskLabel;
+//    private JLabel secondaryContactHighRiskLabel;
+//    private JLabel secondaryContactMediumRiskLabel;
+//    private JLabel secondaryContactLowRiskLabel;
 
     private ResultsTable immediateContactHighRiskTable;
     private ResultsTable immediateContactMediumRiskTable;
     private ResultsTable immediateContactLowRiskTable;
-    private ResultsTable secondaryContactHighRiskTable;
-    private ResultsTable secondaryContactMediumRiskTable;
-    private ResultsTable secondaryContactLowRiskTable;
+//    private ResultsTable secondaryContactHighRiskTable;
+//    private ResultsTable secondaryContactMediumRiskTable;
+//    private ResultsTable secondaryContactLowRiskTable;
 
     public ResultsPanel() {
-        GridLayout layout = new GridLayout(12,1);
+        //GridLayout layout = new GridLayout(12,1); // use for immediate and secondary data
+        GridLayout layout = new GridLayout(6,1);
         setLayout(layout);
         setBorder(new EmptyBorder(0,0,20,0));
 
         immediateContactHighRiskLabel = new JLabel("Immediate Contact - High Risk");
         immediateContactMediumRiskLabel = new JLabel("Immediate Contact - Medium Risk");
         immediateContactLowRiskLabel = new JLabel("Immediate Contact - Low Risk");
-        secondaryContactHighRiskLabel = new JLabel("Secondary Contact - High Risk");
-        secondaryContactMediumRiskLabel = new JLabel("Secondary Contact - Medium Risk");
-        secondaryContactLowRiskLabel = new JLabel("Secondary Contact - Low Risk");
+//        secondaryContactHighRiskLabel = new JLabel("Secondary Contact - High Risk");
+//        secondaryContactMediumRiskLabel = new JLabel("Secondary Contact - Medium Risk");
+//        secondaryContactLowRiskLabel = new JLabel("Secondary Contact - Low Risk");
 
         immediateContactHighRiskTable = new ResultsTable();
         immediateContactMediumRiskTable = new ResultsTable();
         immediateContactLowRiskTable = new ResultsTable();
-        secondaryContactHighRiskTable = new ResultsTable();
-        secondaryContactMediumRiskTable = new ResultsTable();
-        secondaryContactLowRiskTable = new ResultsTable();
+//        secondaryContactHighRiskTable = new ResultsTable();
+//        secondaryContactMediumRiskTable = new ResultsTable();
+//        secondaryContactLowRiskTable = new ResultsTable();
 
         add(immediateContactHighRiskLabel);
         add(immediateContactHighRiskTable);
@@ -47,12 +48,12 @@ public class ResultsPanel extends JPanel {
         add(immediateContactMediumRiskTable);
         add(immediateContactLowRiskLabel);
         add(immediateContactLowRiskTable);
-        add(secondaryContactHighRiskLabel);
-        add(secondaryContactHighRiskTable);
-        add(secondaryContactMediumRiskLabel);
-        add(secondaryContactMediumRiskTable);
-        add(secondaryContactLowRiskLabel);
-        add(secondaryContactLowRiskTable);
+//        add(secondaryContactHighRiskLabel);
+//        add(secondaryContactHighRiskTable);
+//        add(secondaryContactMediumRiskLabel);
+//        add(secondaryContactMediumRiskTable);
+//        add(secondaryContactLowRiskLabel);
+//        add(secondaryContactLowRiskTable);
     }
 
     public void addPersonData(Person person) {
