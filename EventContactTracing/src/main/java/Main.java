fba2a8fed12de29187c9ff1b0630f9212943623c
Kaysi Pilcher <kaysi.pilcher@gmail.com>
2020-12-03 19:41:05 -0700

    updated queries

--- EventContactTracing/src/main/java/Main.java
+++ EventContactTracing/src/main/java/Main.java
@@ -3,32 +3,8 @@ import org.apache.jena.query.*;
 
 public class Main {
 
-    //static String serviceEndpoint = "http://localhost:3030/TestDataset/query";
-    static String serviceEndpoint = "http://ec2-34-205-141-114.compute-1.amazonaws.com:3030/SED/query";
-
     public static void main(String[] args) {
-
         MainFrame mainFrame = new MainFrame("Event Contact Tracing Application");
-        Main main = new Main();
-        main.queryTest();
-    }
-
-    public void queryTest() {
-        String spaqlQuery = "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
-                "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                "\n" +
-                "SELECT DISTINCT ?class ?label ?description\n" +
-                "WHERE {\n" +
-                "  ?class a owl:Class.\n" +
-                "  OPTIONAL { ?class rdfs:label ?label}\n" +
-                "  OPTIONAL { ?class rdfs:comment ?description}\n" +
-                "}\n" +
-                "LIMIT 25";
-
-
-        QueryExecution q = QueryExecutionFactory.sparqlService(serviceEndpoint, spaqlQuery);
-        ResultSet results = q.execSelect();
-        ResultSetFormatter.out(System.out, results);
     }
 
 }
